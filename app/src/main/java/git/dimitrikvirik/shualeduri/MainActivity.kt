7cdfd27d995f60fd98fb3a690c287d19e9ca261a
dito <dimitrikvirik@gmail.com>
2022-05-07 17:08:43 +0400

    Initial commit

---
+++ app/src/main/java/git/dimitrikvirik/shualeduri/MainActivity.kt
@@ -0,0 +1,46 @@
+package git.dimitrikvirik.shualeduri
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.content.edit
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var profileImgButton: ImageView
+    lateinit var profileNextButton: ImageView
+    lateinit var editTextUsername: EditText
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        profileImgButton = findViewById(R.id.profileImgButton)
+        profileNextButton = findViewById(R.id.profileNextButton)
+        editTextUsername = findViewById(R.id.editTextUsername)
+
+        val intent = Intent(this, MainActivity2::class.java)
+
+
+
+        profileImgButton.setOnClickListener {
+            val sharedPref = this.getSharedPreferences("appSharedPref",Context.MODE_PRIVATE)
+
+            sharedPref.edit().putString("username", editTextUsername.text.toString()).apply()
+
+            startActivity(intent)
+        }
+        profileNextButton.setOnClickListener {
+            deleteSharedPreferences("appSharedPref")
+
+            startActivity(intent)
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/git/dimitrikvirik/shualeduri/MainActivity2.kt
@@ -0,0 +1,37 @@
+package git.dimitrikvirik.shualeduri
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+
+    lateinit var profileBackButton: ImageView
+    lateinit var textUsername: TextView
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        profileBackButton = findViewById(R.id.profileBackButton)
+        textUsername = findViewById(R.id.textUsername)
+
+        val username = this.getSharedPreferences("appSharedPref",MODE_PRIVATE).getString("username", "Your name")
+
+        textUsername.text = "Hello, $username"
+
+
+
+        profileBackButton.setOnClickListener {
+            finish()
+        }
+
+    }
+}
